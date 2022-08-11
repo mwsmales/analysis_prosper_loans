dfb237bdeb34838f5c719f21cc822ee5af160dca
Michael S <michael.smales@gmail.com>
2022-08-11 12:15:58 -0700

    updated plot_functions to use object-oriented matplotlib style

--- plot_functions.py
+++ plot_functions.py
@@ -42,8 +42,7 @@ def hist_plot(chart_data, xlabel=None, ylabel=None, title=None, bin_step=None, m
             tick_labels.append("{:.2f}".format(label))
 
     # create histoagram plot
-    fig = plt.figure(figsize=[8,5]);
-    ax = fig.add_axes([0.1, 0.1, 0.9, 0.9]);
+    fig, ax = plt.subplots(figsize=[8,5])
     ax.hist(chart_data, bins=bin_edges);
 
     # set x-axis ticks and labels, offsetting to center of each bin
@@ -53,9 +52,9 @@ def hist_plot(chart_data, xlabel=None, ylabel=None, title=None, bin_step=None, m
 
     # add labels etc
     sns.despine()
-    plt.xlabel(xlabel);
-    plt.ylabel(ylabel);
-    plt.title(title);
+    ax.set_xlabel(xlabel);
+    ax.set_ylabel(ylabel);
+    ax.set_title(title);
 
     # set xlimit
     ax.set_xlim(xlim)
@@ -79,27 +78,28 @@ def tick_format(axis='x', format_type='pct'):
         format_string = '{:.1f}bn'
         format_multiple = 0.000000001
 
+    ax = plt.gca()
+
     if axis == 'x':
         xtick_locs, xtick_labels = plt.xticks()
         xtick_labels=[]
         for loc in xtick_locs:
             xtick_labels.append(format_string.format(loc*format_multiple))
-        plt.xticks(ticks=xtick_locs, labels=xtick_labels);
+        ax.set_xticks(ticks=xtick_locs, labels=xtick_labels);
     elif axis == 'y':
         ytick_locs, ytick_labels = plt.yticks()
         ytick_labels=[]
         for loc in ytick_locs:
             ytick_labels.append(format_string.format(loc*format_multiple))
-        plt.yticks(ticks=ytick_locs, labels=ytick_labels);
+        ax.set_yticks(ticks=ytick_locs, labels=ytick_labels);
 
 
 
 # define function for plotting a (vertical) column chart with relative y axis
 def rel_col_chart(chart_data, xlabel=None, ylabel=None, title=None, plot_order=None):
     base_color = sns.color_palette()[0]
-    plt.figure(figsize=[8,5])
-
-    sns.countplot(x=chart_data, color=base_color, order=plot_order);
+    fig, ax = plt.subplots(figsize=[8,5])
+    sns.countplot(x=chart_data, color=base_color, order=plot_order, ax=ax);
 
     # set up y axis which goes up to 100%
     y_ticks_max = chart_data.value_counts().max() / chart_data.count()
@@ -111,29 +111,30 @@ def rel_col_chart(chart_data, xlabel=None, ylabel=None, title=None, plot_order=N
     y_tick_names = ['{:0.0f}%'.format(v * 100) for v in y_tick_props]
 
     # set x and y labels
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-
-    ax = plt.gca()
-    ax.set_yticks(y_tick_props * chart_data.count(), labels=y_tick_names)
     sns.despine();
+    ax.set_xlabel(xlabel);
+    ax.set_ylabel(ylabel);
+    ax.set_title(title);
+
+    # set ticks etc
+    ax.set_yticks(y_tick_props * chart_data.count(), labels=y_tick_names)
 
 
 
 # define function for plotting a (horizontal) bar chart with relative x axis
 def rel_bar_chart(chart_data, xlabel=None, ylabel=None, title=None, plot_order=[]):
-    base_color = sns.color_palette()[0]
-    plt.figure(figsize=[8,5])
-
     # convert plot order to a list if needed
     if isinstance(plot_order, pd.Index):
         plot_order = list(plot_order.values)
 
+    # if plot order is not provided, take the top 10 categories ranked largest to smallest
     if plot_order == []:
-        sns.countplot(y=chart_data, color=base_color);
-    else:
-        sns.countplot(y=chart_data, color=base_color, order=plot_order);
+        plot_order = chart_data.value_counts().index.to_list()[:10]
+
+    base_color = sns.color_palette()[0]
+    # plt.figure(figsize=[8,5])
+    fix, ax = plt.subplots(figsize=[8,5])
+    sns.countplot(y=chart_data, color=base_color, order=plot_order, ax=ax);
 
     # set up y axis which goes up to 100%
     x_ticks_max = chart_data.value_counts().max() / chart_data.count()
@@ -145,40 +146,38 @@ def rel_bar_chart(chart_data, xlabel=None, ylabel=None, title=None, plot_order=[
     x_tick_names = ['{:0.0f}%'.format(v * 100) for v in x_tick_props]
 
     # set x and y labels
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-
-    ax = plt.gca()
-    ax.set_xticks(x_tick_props * chart_data.count(), labels=x_tick_names)
     sns.despine();
+    ax.set_xlabel(xlabel);
+    ax.set_ylabel(ylabel);
+    ax.set_title(title);
+
+    # set tick labels
+    ax.set_xticks(x_tick_props * chart_data.count(), labels=x_tick_names)
 
     # Logic to print the proportion text on the bars
     counts = chart_data.value_counts()
-    if plot_order == []:
-        plot_order = chart_data.unique()
 
     for i in range(len(plot_order)):
         count = counts.loc[plot_order[i]]
         # Convert count into a percentage, and then into string
         pct_string = '{:0.1f}%'.format(100 * count / chart_data.count())
         # Print the string value on the bar. 
-        plt.text(count + counts.max()*0.01, i, pct_string, va='center')
-
+        ax.text(count + counts.max()*0.01, i, pct_string, va='center')
 
 
 # function to plot heatmap of counts from dataframe, with relative scale (i.e. counts add to 100%)
 def count_heatmap_relative(data, xrange, yrange, xlabel=None, ylabel=None, title=None, xbin_step=None, ybin_step=None):
     
     df=data
-    plt.figure(figsize=[9,7])
+    fig, ax = plt.subplots(figsize=[9,7])
+    # plt.figure(figsize=[9,7])
 
     # arrange bin edges and tick locations
     x_bin_edges = np.arange(0, df[xrange].max() + xbin_step, xbin_step)
     y_bin_edges = np.arange(0, df[yrange].max() + ybin_step, ybin_step)
 
     # plot histogram
-    hist, xbins, ybins, im = plt.hist2d(
+    hist, xbins, ybins, im = ax.hist2d(
         data = df, 
         x = xrange, 
         y = yrange, 
@@ -188,8 +187,8 @@ def count_heatmap_relative(data, xrange, yrange, xlabel=None, ylabel=None, title
     );
 
     # align x and y ticks to bin edges
-    plt.xticks(x_bin_edges)
-    plt.yticks(y_bin_edges)
+    ax.set_xticks(x_bin_edges)
+    ax.set_yticks(y_bin_edges)
 
     # add text to histogram with scale set to 100%
     hist = hist / len(df) * 100
@@ -198,21 +197,21 @@ def count_heatmap_relative(data, xrange, yrange, xlabel=None, ylabel=None, title
             if pd.isna(hist.T[i,j]):
                 pass
             elif hist.T[i,j] > 10:
-                plt.text(xbins[j]+xbin_step/5, ybins[i]+ybin_step/2, '{:.0f}%'.format(hist.T[i,j]), 
+                ax.text(xbins[j]+xbin_step/5, ybins[i]+ybin_step/2, '{:.0f}%'.format(hist.T[i,j]), 
                         color="w", ha="left", va="center", fontweight='bold')
             else:
-                plt.text(xbins[j]+xbin_step/5, ybins[i]+ybin_step/2, '{:.0f}%'.format(hist.T[i,j]), 
+                ax.text(xbins[j]+xbin_step/5, ybins[i]+ybin_step/2, '{:.0f}%'.format(hist.T[i,j]), 
                         color="k", ha="left", va="center")
 
     # add title and axis labels
-    plt.title(title, loc='left');
-    plt.xlabel(xlabel);
-    plt.ylabel(ylabel);
+    sns.despine();
+    ax.set_title(title, loc='left');
+    ax.set_xlabel(xlabel);
+    ax.set_ylabel(ylabel);
 
     # format tick labels
     tick_format(axis='x', format_type='k')
     tick_format(axis='y', format_type='pct')
 
     # apply some aesthetic formatting
-    plt.grid(which="major", color="w", linestyle='-', linewidth=5)
-    sns.despine();
+    ax.grid(which="major", color="w", linestyle='-', linewidth=5)
